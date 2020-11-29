0ae8aa820525d7478ba5b26c7baff487a38afa26
Lizbeth Baez <lizbaez.140799@gmail.com>
2020-11-29 11:23:14 -0700

    Cambios ProyectoFinal

--- proyectoFinal/ViewController.swift
+++ proyectoFinal/ViewController.swift
@@ -10,19 +10,31 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    var noticias : [Noticias] = []
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return noticias.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let celdaNoticia = tableView.dequeueReusableCell(withIdentifier: "celdaNoticia") as! celdaNoticiaController
+        
         return celdaNoticia
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        noticias.append(Noticias(texto: "Noticia1 aqui karlita", fecha: "30 / 10 / 20", imagen: <#T##String#>))
+        noticias.append(Noticias(texto: "Noticia2 aqui karlita", fecha: "30 / 10 / 20", imagen: <#T##String#>))
+        noticias.append(Noticias(texto: "Noticia3 aqui karlita", fecha: "30 / 10 / 20", imagen: <#T##String#>))
+        noticias.append(Noticias(texto: "Noticia4 aqui karlita", fecha: "30 / 10 / 20", imagen: String))
+        noticias.append(Noticias(texto: "Noticia5 aqui karlita", fecha: "30 / 10 / 20", imagen: <#T##String#>))
     }
 
 
